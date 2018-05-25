dce7c95cce6013d76f30788c3ed6ad3a6020b054
PastLeo <chgu82837@gmail.com>
2018-05-25 11:44:26 +0800

    complete workshop
    memorizing start, answer, continue and routes

--- words-cards/app/controllers/memorizing_controller.rb
+++ words-cards/app/controllers/memorizing_controller.rb
@@ -1,5 +1,23 @@
 class MemorizingController < ApplicationController
 
   def start
+    @group = params['group']
+    Word.where(group: @group).update_all(visited: false)
+  end
+
+  def continue
+    @group = params['group']
+    @word = Word.where(group: @group, visited: false).order("RANDOM()").first
+    if @word.present?
+      @word.visited = true
+      @word.save
+    else
+      redirect_to words_path, notice: "memorizing #{@group} finished!"
+    end
+  end
+
+  def answer
+    @group = params['group']
+    @word = Word.find(params['word_id'])
   end
 end
--- words-cards/config/routes.rb
+++ words-cards/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/start/:group', to: 'memorizing#start', as: 'start'
+  get '/continue/:group', to: 'memorizing#continue', as: 'continue'
+  get '/answer/:group/:word_id', to: 'memorizing#answer', as: 'answer'
 end
